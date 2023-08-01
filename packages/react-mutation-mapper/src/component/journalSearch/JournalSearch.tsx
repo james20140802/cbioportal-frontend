8ed5a6224b7d6b4de1b80ef89bbd5842b739ce8f
SangChu04 <drchasekim@gmail.com>
2023-08-01 17:47:43 +0900

    feat: Add lines for the situation when fetching is complete

--- packages/react-mutation-mapper/src/component/journalSearch/JournalSearch.tsx
+++ packages/react-mutation-mapper/src/component/journalSearch/JournalSearch.tsx
@@ -6,9 +6,11 @@ import journalSearchLogoSrc from '../../images/journal-search.svg';
 import annotationStyles from '../column/annotation.module.scss';
 import { makeObservable, observable } from 'mobx';
 import { errorIcon, loaderIcon } from '../StatusHelpers';
+import { IJournalSearch } from 'cbioportal-utils';
 
 export interface IJournalSearchProps {
     journalSearchStatus: 'pending' | 'error' | 'complete';
+    journalSearch: IJournalSearch[];
 }
 
 @observer
@@ -35,6 +37,17 @@ export default class JournalSearch extends React.Component<
 
         if (this.props.journalSearchStatus == 'error') {
             journalSearchContent = errorIcon('Error fetching journal data');
+        } else if (this.props.journalSearch !== undefined) {
+            journalSearchContent = (
+                <span className={`${annotationStyles['annotation-item']}`}>
+                    <img
+                        width={journalSearchImgWidth}
+                        height={journalSearchImgHeight}
+                        src={journalSearchImgSrc}
+                        alt="Journal Search Result"
+                    />
+                </span>
+            );
         } else {
             journalSearchContent = loaderIcon('pull-left');
         }
