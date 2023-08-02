6dd845f2cf83b0c128075b4f711ea58254a41150
SangChu04 <drchasekim@gmail.com>
2023-08-02 13:02:56 +0900

    feat: Add tooltip for journalSearch Annotation

--- packages/react-mutation-mapper/src/component/journalSearch/JournalSearch.tsx
+++ packages/react-mutation-mapper/src/component/journalSearch/JournalSearch.tsx
@@ -7,10 +7,37 @@ import annotationStyles from '../column/annotation.module.scss';
 import { makeObservable, observable } from 'mobx';
 import { errorIcon, loaderIcon } from '../StatusHelpers';
 import { IJournalSearch } from 'cbioportal-utils';
+import { DefaultTooltip } from 'cbioportal-frontend-commons';
+import { placeArrow } from '../myCancerGenome/MyCancerGenome';
+import journalSearchStyles from './journalSearch.module.scss';
+import { hideArrow } from '../civic/Civic';
 
 export interface IJournalSearchProps {
     journalSearchStatus: 'pending' | 'error' | 'complete';
-    journalSearch: IJournalSearch[];
+    journalSearch: IJournalSearch[] | null | undefined;
+}
+
+export function journalSearchLinks(journalSearches: IJournalSearch[]) {
+    const links: any[] = [];
+    journalSearches.forEach((search, index) => {
+        links.push(
+            <li key={index}>
+                <a href={search.linkHTML} target="_blank">
+                    {search.author}
+                    <br />
+                    <i>{search.title}</i>
+                </a>
+            </li>
+        );
+    });
+
+    return (
+        <span>
+            <b>Journals related to {journalSearches[0].hugoGeneSymbol}:</b>
+            <br />
+            <ul className={journalSearchStyles['link-list']}>{links}</ul>
+        </span>
+    );
 }
 
 @observer
@@ -37,17 +64,38 @@ export default class JournalSearch extends React.Component<
 
         if (this.props.journalSearchStatus == 'error') {
             journalSearchContent = errorIcon('Error fetching journal data');
-        } else if (this.props.journalSearch !== undefined) {
-            journalSearchContent = (
-                <span className={`${annotationStyles['annotation-item']}`}>
-                    <img
-                        width={journalSearchImgWidth}
-                        height={journalSearchImgHeight}
-                        src={journalSearchImgSrc}
-                        alt="Journal Search Result"
-                    />
-                </span>
-            );
+        } else if (this.props.journalSearchStatus == 'complete') {
+            if (
+                this.props.journalSearch !== null &&
+                this.props.journalSearch !== undefined
+            ) {
+                const arrowContent = <div className="rc-tooltip-arrow-inner" />;
+                const toolttipContent = journalSearchLinks(
+                    this.props.journalSearch
+                );
+                journalSearchContent = (
+                    <span className={`${annotationStyles['annotation-item']}`}>
+                        <img
+                            width={journalSearchImgWidth}
+                            height={journalSearchImgHeight}
+                            src={journalSearchImgSrc}
+                            alt="Journal Search Result"
+                        />
+                    </span>
+                );
+
+                journalSearchContent = (
+                    <DefaultTooltip
+                        overlay={toolttipContent}
+                        placement="right"
+                        trigger={['hover', 'focus']}
+                        onPopupAlign={hideArrow}
+                        destroyTooltipOnHide={false}
+                    >
+                        {journalSearchContent}
+                    </DefaultTooltip>
+                );
+            }
         } else {
             journalSearchContent = loaderIcon('pull-left');
         }
